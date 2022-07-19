bc0c64797f388ffb830c92f1982adf624eaa6ea9
huangdijia <huangdijia@gmail.com>
2022-07-19 22:33:36 +0800

    Adds loadBalances

---
+++ src/LoadBalancer/WeightedRandom.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of jet.
+ *
+ * @link     https://github.com/friendsofhyperf/jet
+ * @document https://github.com/friendsofhyperf/jet/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ * @license  https://github.com/friendsofhyperf/jet/blob/main/LICENSE
+ */
+namespace FriendsOfHyperf\Jet\LoadBalancer;
+
+use FriendsOfHyperf\Jet\Exception\NoNodesAvailableException;
+
+class WeightedRandom extends AbstractLoadBalancer
+{
+    /**
+     * Select an item via the load balancer.
+     */
+    public function select(array ...$parameters): Node
+    {
+        $totalWeight = 0;
+        $isSameWeight = true;
+        $lastWeight = null;
+        $nodes = $this->nodes;
+        foreach ($nodes as $node) {
+            if (! $node instanceof Node) {
+                continue;
+            }
+            $weight = $node->weight;
+            $totalWeight += $weight;
+            if ($lastWeight !== null && $isSameWeight && $weight !== $lastWeight) {
+                $isSameWeight = false;
+            }
+            $lastWeight = $weight;
+        }
+        if ($totalWeight > 0 && ! $isSameWeight) {
+            $offset = mt_rand(0, $totalWeight - 1);
+            foreach ($nodes as $node) {
+                $offset -= $node->weight;
+                if ($offset < 0) {
+                    return $node;
+                }
+            }
+        }
+
+        if (! $nodes) {
+            throw new NoNodesAvailableException('Cannot select any node from load balancer.');
+        }
+
+        return $nodes[array_rand($nodes)];
+    }
+}
---
+++ src/LoadBalancer/WeightedRoundRobin.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of jet.
+ *
+ * @link     https://github.com/friendsofhyperf/jet
+ * @document https://github.com/friendsofhyperf/jet/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ * @license  https://github.com/friendsofhyperf/jet/blob/main/LICENSE
+ */
+namespace FriendsOfHyperf\Jet\LoadBalancer;
+
+use FriendsOfHyperf\Jet\Exception\NoNodesAvailableException;
+use MathPHP\Algebra;
+
+class WeightedRoundRobin extends AbstractLoadBalancer
+{
+    private int $lastNode = 0;
+
+    private int $currentWeight = 0;
+
+    /**
+     * Select an item via the load balancer.
+     */
+    public function select(array ...$parameters): Node
+    {
+        $count = count($this->nodes);
+        if ($count <= 0) {
+            throw new NoNodesAvailableException('Cannot select any node from load balancer.');
+        }
+        $maxWeight = $this->maxWeight($this->nodes);
+        while (true) {
+            $this->lastNode = ($this->lastNode + 1) % $count;
+            if ($this->lastNode === 0) {
+                $this->currentWeight = $this->currentWeight - $this->gcd($this->nodes);
+                if ($this->currentWeight <= 0) {
+                    $this->currentWeight = $maxWeight;
+                    if ($this->currentWeight == 0) {
+                        // Degrade to random algorithm.
+                        return $this->nodes[array_rand($this->nodes)];
+                    }
+                }
+            }
+            /** @var Node $node */
+            $node = $this->nodes[$this->lastNode];
+            if ($node->weight >= $this->currentWeight) {
+                return $node;
+            }
+        }
+    }
+
+    /**
+     * Calculate the max weight of nodes.
+     */
+    private function maxWeight(iterable $nodes): int
+    {
+        $max = null;
+        foreach ($nodes as $node) {
+            if (! $node instanceof Node) {
+                continue;
+            }
+            if ($max === null) {
+                $max = $node->weight;
+            } else {
+                $max = max($max, $node->weight);
+            }
+        }
+        return $max;
+    }
+
+    /**
+     * Calculate the gcd of nodes.
+     */
+    private function gcd(iterable $nodes): int
+    {
+        $x = $y = null;
+        foreach ($nodes as $node) {
+            if (! $node instanceof Node) {
+                continue;
+            }
+            if ($x === null) {
+                $x = $node->weight;
+                continue;
+            }
+            $y = $node->weight;
+            $x = Algebra::gcd($x, $y);
+        }
+        return $x;
+    }
+}
