50ec5017b85b7d707adc0ac5635edceb66b682b2
Deeka Wong <huangdijia@gmail.com>
2022-07-20 17:12:39 +0800

    AddsThrow NoNodesAvailableException when cannot select any node

---
+++ src/Exception/NoNodesAvailableException.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of jet.
+ *
+ * @link     https://github.com/friendsofhyperf/jet
+ * @document https://github.com/friendsofhyperf/jet/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ * @license  https://github.com/friendsofhyperf/jet/blob/main/LICENSE
+ */
+namespace FriendsOfHyperf\Jet\Exception;
+
+use RuntimeException;
+
+class NoNodesAvailableException extends RuntimeException
+{
+}
