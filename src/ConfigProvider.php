8a8e57f17bc150357384f266947a42f66acaf163
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-08-10 23:23:12 +0800

    feat: Add strict types declaration and file header to ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -1,5 +1,14 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of jet.
+ *
+ * @link     https://github.com/friendsofhyperf/jet
+ * @document https://github.com/friendsofhyperf/jet/3.x/main/README.md
+ * @contact  Huangdijia@gmail.com
+ */
+
 namespace FriendsOfHyperf\Jet;
 
 class ConfigProvider
