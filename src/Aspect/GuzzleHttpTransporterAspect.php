39530a3a967d627baf0b17ad3094c26fe1adadcb
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-08-10 23:09:04 +0800

    feat: Add GuzzleHttpTransporterAspect

--- src/Aspect/GuzzleHttpTransporterAspect.php
+++ src/Aspect/GuzzleHttpTransporterAspect.php
@@ -1,5 +1,14 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of jet.
+ *
+ * @link     https://github.com/friendsofhyperf/jet
+ * @document https://github.com/friendsofhyperf/jet/3.x/main/README.md
+ * @contact  Huangdijia@gmail.com
+ */
+
 namespace FriendsOfHyperf\Jet\Aspect;
 
 use Hyperf\Di\Aop\AbstractAspect;
@@ -29,9 +38,8 @@ class GuzzleHttpTransporterAspect extends AbstractAspect
     {
         /** @var \FriendsOfHyperf\Jet\Transporter\GuzzleHttpTransporter $instance */
         $instance = $proceedingJoinPoint->getInstance();
-        $config = (function() { return $this->config; })->call($instance);
+        $config = (function () { return $this->config; })->call($instance);
 
         return $this->clientFactory->create($config);
     }
-
 }
