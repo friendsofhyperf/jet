d34146f8f0e025daf2f291bffd2cc5ecb8e586cc
Deeka Wong <huangdijia@gmail.com>
2024-08-11 16:44:22 +0800

    Split Arr and Str (#19)
    * Split Arr and Str
    
    Co-Authored-By: Deeka Wong <8337659+huangdijia@users.noreply.github.com>
    
    * feat: Update path generation to handle namespaces with backslashes
    
    * feat: Fix backslash handling in path generation
    
    ---------
    
    Co-authored-by: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

---
+++ src/Support/Str.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of friendsofhyperf/jet.
+ *
+ * @link     https://github.com/friendsofhyperf/jet
+ * @document https://github.com/friendsofhyperf/jet/blob/main/README.md
+ * @contact  huangdijia@gmail.com
+ */
+
+namespace FriendsOfHyperf\Jet\Support;
+
+class Str
+{
+    /**
+     * @param string $value
+     * @param string $delimiter
+     * @return mixed
+     */
+    public static function snake($value, $delimiter = '_')
+    {
+        if (! ctype_lower($value)) {
+            $value = preg_replace('/\s+/u', '', ucwords($value));
+            $value = static::lower(preg_replace('/(.)(?=[A-Z])/u', '$1' . $delimiter, $value));
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    public static function lower($value)
+    {
+        return mb_strtolower($value, 'UTF-8');
+    }
+
+    /**
+     * @param string $value
+     * @param string $gap
+     * @return string
+     */
+    public static function studly($value, $gap = '')
+    {
+        $value = ucwords(str_replace(['-', '_'], ' ', $value));
+
+        return str_replace(' ', $gap, $value);
+    }
+
+    /**
+     * @param string $search
+     * @param array|string $replace
+     * @param string $subject
+     * @return string
+     */
+    public static function replaceFirst($search, $replace, $subject)
+    {
+        if ($search == '') {
+            return $subject;
+        }
+
+        $position = strpos($subject, $search);
+
+        if ($position !== false) {
+            return substr_replace($subject, $replace, $position, strlen($search));
+        }
+
+        return $subject;
+    }
+
+    /**
+     * @param string $search
+     * @param iterable<string> $replace
+     * @param string $subject
+     * @return string
+     */
+    public static function replaceArray($search, $replace, $subject)
+    {
+        foreach ($replace as $value) {
+            $subject = self::replaceFirst($search, $value, $subject);
+        }
+
+        return $subject;
+    }
+}
