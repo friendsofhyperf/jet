1126f7dfa9ac1d3a59e56875c38352e1dec00f30
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-08-11 10:25:53 +0800

    Cs fix

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * This file is part of friendsofhyperf/jet.
  *
  * @link     https://github.com/friendsofhyperf/jet
- * @document https://github.com/friendsofhyperf/jet/blob/4.x/README.md
+ * @document https://github.com/friendsofhyperf/jet/blob/main/README.md
  * @contact  huangdijia@gmail.com
  */
 use Huangdijia\PhpCsFixer\Config;
@@ -16,7 +16,7 @@ return (new Config())
     ->setHeaderComment(
         projectName: 'friendsofhyperf/jet',
         projectLink: 'https://github.com/friendsofhyperf/jet',
-        projectDocument: 'https://github.com/friendsofhyperf/jet/blob/4.x/README.md',
+        projectDocument: 'https://github.com/friendsofhyperf/jet/blob/main/README.md',
         contacts: [
             'huangdijia@gmail.com',
         ],
--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * This file is part of friendsofhyperf/jet.
  *
  * @link     https://github.com/friendsofhyperf/jet
- * @document https://github.com/friendsofhyperf/jet/blob/4.x/README.md
+ * @document https://github.com/friendsofhyperf/jet/blob/main/README.md
  * @contact  huangdijia@gmail.com
  */
 ! defined('BASE_PATH') && define('BASE_PATH', __DIR__);
