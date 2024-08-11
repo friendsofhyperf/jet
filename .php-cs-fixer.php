ac30e2f15ed8df6e7af86253a9f999b4b2cb93c4
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-08-11 16:16:03 +0800

    Cs Fix
    Co-Authored-By: Deeka Wong <8337659+huangdijia@users.noreply.github.com>

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,12 +1,11 @@
 <?php
 
 $header = <<<'EOF'
-This file is part of jet.
+This file is part of friendsofhyperf/jet.
 
 @link     https://github.com/friendsofhyperf/jet
 @document https://github.com/friendsofhyperf/jet/blob/main/README.md
 @contact  huangdijia@gmail.com
-@license  https://github.com/friendsofhyperf/jet/blob/main/LICENSE
 EOF;
 
 return (new PhpCsFixer\Config())
