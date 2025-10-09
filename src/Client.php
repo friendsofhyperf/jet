55ccd3c22367df2fb7b21e24efaab7279e08f823
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-10-09 10:31:07 +0800

    Fix: Ensure unpacked data is treated as an array and improve error handling in Client

--- src/Client.php
+++ src/Client.php
@@ -70,16 +70,12 @@ class Client
                 throw new RecvFailedException('Recv failed');
             }
 
-            return with($packer->unpack($ret), function ($data) use ($ret) {
-                if (! is_array($data)) {
-                    throw new RecvFailedException('Recv failed, invalid data: ' . $ret);
-                }
-
+            return with((array) $packer->unpack($ret), function ($data) use ($ret) {
                 if (array_key_exists('result', $data)) {
                     return $data['result'];
                 }
 
-                throw new ServerException($data['error'] ?? []);
+                throw new ServerException($data['error'] ?? ['code' => 0, 'message' => 'Recv failed, invalid data: ' . $ret]);
             });
         };
 
